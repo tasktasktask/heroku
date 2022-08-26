45f865365977405c2891ff62b03a2d7e410ab643
tasuku <tasukukawaguchi0516@gmail.com>
2022-08-26 16:14:43 +0900

    index.phpとapp.jsを編集:git branch 練習

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 echo 'hello';
 echo 'hello';
+?>
+
+<html>
+  <meta charset="UTF-8">
+  <meta name="description" content="test">
+  <title>Test|task</title>
+  <body>
+    <script type="text/javascript" src="app.js"></script>
+  </body>
+</html>
